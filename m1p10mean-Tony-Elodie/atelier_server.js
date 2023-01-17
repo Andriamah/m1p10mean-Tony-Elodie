a7d467edfad2a81a430072d325d1d5a7ee1c332f
tonyfenoaina <tfenoaina@gmail.com>
2023-01-17 10:20:44 +0300

    restructuraton code

--- m1p10mean-Tony-Elodie/atelier_server.js
+++ m1p10mean-Tony-Elodie/atelier_server.js
@@ -1,39 +1,34 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
 const app = express()
-var router = express.Router()
 require('dotenv')
 
-function start(app = express()) {
+function start(app = express(), db) {
   // module.exports = router;
-
-
-const connectionString = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb"
-
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('garage')
-      const utilisateurCollection = db.collection('voiture')
-
-    // ========================
-    // Middlewares
-    // ========================
-
-  //  app.set('view engine', 'ejs')
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.use(bodyParser.json())
-    app.use(express.static('public'))
-
-    // ========================
-    // Routes
-    // ========================
-
-   
+  const voiturecollection = db.collection('voiture')
+  const reparationcollection = db.collection('reparation')
+  // ========================
+  // Middlewares
+  // ========================
+  app.get('/reception_voiture', (req, res) => {
+    voiturecollection.find({ statut: "0" }).toArray()
+      .then(quotes => {
+        res.json(quotes);
+      })
+      .catch(/* ... */)
   })
-  .catch(console.error)
-  
+
+  app.post('/ajouter_reparation', (req, res) => {
+    console.log("hahahaha")
+    var detail = req.body.detail
+    var total = 0;
+    for (let index = 0; index < detail; index++) {
+      console.log(detail[index].prix)
+    }
+
+    // reparationcollection.insertOne(req.body);
+
+  })
 }
 
 exports.start = start;
