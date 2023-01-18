551db1353beceabdc5e0730b0016e134b9a7d2cc
Andriamah <80816142+Andriamah@users.noreply.github.com>
2023-01-18 21:22:01 +0100

    Essai routage

--- front-angular/frontGarage/src/app/app-routing.module.ts
+++ front-angular/frontGarage/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// import { CrisisListComponent } from './crisis-list/crisis-list.component';
+// import { HeroListComponent } from './hero-list/hero-list.component';
+import { UtilisateurListComponent } from './utilisateur-list/utilisateur-list.component';
+
+
+const appRoutes: Routes = [
+  { path: 'utilisateur', component: UtilisateurListComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
