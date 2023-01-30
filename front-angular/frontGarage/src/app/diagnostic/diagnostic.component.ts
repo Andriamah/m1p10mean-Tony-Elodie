dd770c5cdd20d4a81d0e14b56354a15e16ad6ea2
tonyfenoaina <tfenoaina@gmail.com>
2023-01-30 16:12:43 +0300

    Update Reparation + design atelier

--- front-angular/frontGarage/src/app/diagnostic/diagnostic.component.ts
+++ front-angular/frontGarage/src/app/diagnostic/diagnostic.component.ts
@@ -14,36 +14,37 @@ import { VoitureService } from '../Service/voiture.service';
 export class DiagnosticComponent {
 
   name = 'Angular';
-  
+
   productForm: FormGroup;
-  voiture!: Voiture ;
-  matricule = this.route.snapshot.paramMap.get("voiture")??"";
-  tableau : Detail[]=[]
+  voiture!: Voiture;
+  matricule = this.route.snapshot.paramMap.get("voiture") ?? "";
+  tableau: Detail[] = []
+  start: number = 0
   // jsonString!: string  
 
-  async getVoiture(){
-    (await this.voitureservice.getOnevoiture(this.matricule??"")).subscribe({
+  async getVoiture() {
+    (await this.voitureservice.getOnevoiture(this.matricule ?? "")).subscribe({
       next: (data) => {
         this.voiture = data
-        console.log("hahahahahahahaha"+data)
-        console.log("tableau"+this.tableau)
+        console.log("hahahahahahahaha" + data)
+        console.log("tableau" + this.tableau)
       }
     })
-  } 
+  }
   async getreparations() {
-        // let data = JSON.parse(this.voiture.matricule??"");
-        // this.voiture = <Voiture>data;
-      (await this.reparationservice.getReparation(this.matricule)).subscribe({
-        next: (data) => {
-          this.tableau = data
-          console.log("tyhakakaka"+data)
-        }
-      })
-    }
-  constructor(private fb: FormBuilder,private reparationservice: ReparationService,private route: ActivatedRoute,private voitureservice:VoitureService) {
-     this.getreparations();
+    // let data = JSON.parse(this.voiture.matricule??"");
+    // this.voiture = <Voiture>data;
+    (await this.reparationservice.getReparation(this.matricule)).subscribe({
+      next: (data) => {
+        this.tableau = data
+        console.log("tyhakakaka" + data)
+      }
+    })
+  }
+  constructor(private fb: FormBuilder, private reparationservice: ReparationService, private route: ActivatedRoute, private voitureservice: VoitureService) {
+    this.getreparations();
     //  await this.getreparations()
-     console.log("ato zah"+ this.tableau)
+    console.log("ato zah" + this.tableau)
     this.productForm = this.fb.group({
       detail: this.fb.array(this.tableau),
     });
@@ -67,31 +68,37 @@ export class DiagnosticComponent {
   removeQuantity(i: number) {
     this.detail().removeAt(i);
   }
-
   onSubmit() {
     console.log(this.productForm.value);
     // console.log("ty leizy"+JSON.stringify(this.voiture).replace("[",""));
-    console.log("atooo"+JSON.stringify(this.productForm.value.detail))
-    this.reparationservice.AjoutReparation(this.voiture,this.productForm.value.detail);
+    console.log("atooo" + JSON.stringify(this.productForm.value.detail))
+    this.reparationservice.AjoutReparation(this.voiture, this.productForm.value.detail);
   }
 
   async ngOnInit(): Promise<void> {
-     (await this.reparationservice.getReparation(this.matricule)).subscribe({
-      next: (data:  Detail[]) => {
-        this.productForm.value.detail= data
-        this.productForm = this.fb.group({
-          detail: this.fb.array(data),
-        });
-        console.log("tonyyyyyy"+data)
-      }
-    })
-     await this.getreparations().then(
-      async value => this.productForm.value.detail=this.tableau,
-      // result =>this.productForm.setValue(this.tableau), 
-      affiche =>console.log("eto aloha"+this.tableau),  
-     )
-     await this.getVoiture()
-    
+    for (let i = 0; i < 50; i++) {
+      (await this.reparationservice.getReparation(this.matricule)).subscribe({
+        next: async (data: Detail[]) => {
+          this.productForm.value.detail = data
+          this.productForm = this.fb.group({
+            detail: this.fb.array(data),
+          });
+          console.log("tonyyyyyy" + data)
+        }
+      })
+
+
+    }
+
+
+
+    //  await this.getreparations().then(
+    //   async value => this.productForm.value.detail=this.tableau,
+    //   // result =>this.productForm.setValue(this.tableau), 
+    //   affiche =>console.log("eto aloha"+this.tableau),  
+    //  )
+    await this.getVoiture()
+
   }
 
 }
