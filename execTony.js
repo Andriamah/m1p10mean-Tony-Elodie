ad0260fb83be016a982522c02e8542ddfe0e9380
Andriamah <80816142+Andriamah@users.noreply.github.com>
2023-01-30 07:57:52 +0100

    Update exec Tony

--- execTony.js
+++ execTony.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
-const cors=require('cors');
-const connectionString = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb"
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use(cors())
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-.then(client => {
-  console.log('Connected to Database')
-  const db = client.db('garage')
-  var atelier_server = require('./atelier_server')
-  atelier_server.start(app,db);
-})
-.catch(console.error)
+// const express = require('express')
+// const app = express()
+// const bodyParser = require('body-parser')
+// const MongoClient = require('mongodb').MongoClient
+// const cors=require('cors');
+// const connectionString = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb"
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+// app.use(express.static('public'))
+// app.use(cors())
+// MongoClient.connect(connectionString, { useUnifiedTopology: true })
+// .then(client => {
+//   console.log('Connected to Database')
+//   const db = client.db('garage')
+//   var atelier_server = require('./atelier_server')
+//   atelier_server.start(app,db);
+// })
+// .catch(console.error)
 
-const isProduction = process.env.NODE_ENV === 'production'
-const port = isProduction ? 7500 : 3000
-app.listen(port, function () {
-    console.log(`listening on ${port}`)    
-})
+// const isProduction = process.env.NODE_ENV === 'production'
+// const port = isProduction ? 7500 : 3000
+// app.listen(port, function () {
+//     console.log(`listening on ${port}`)    
+// })
